ae8948f860fd13ec73ebb31140408df6b3dc5684
Juli1ou2 <julien.mrss@gmail.com>
2025-07-02 10:00:39 +0200

    feat(chatbot): add style

--- app.py
+++ app.py
@@ -6,3 +6,10 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template('index.html')
+
+# @app.route('/login', methods=['GET', 'POST'])
+# def login():
+#     if request.method == 'POST':
+#         return do_the_login()
+#     else:
+#         return show_the_login_form()
